645d6d5c9aa22b9b2bd919fc1bb155e03049c243
Breezy <114720008+breezy-codes@users.noreply.github.com>
2024-09-18 09:56:00 +1000

    Peer Review

--- 14_SK_camera_OOP_peer_review/program_files/Code1/obj/Debug/net8.0/Code1.AssemblyInfo.cs
+++ 14_SK_camera_OOP_peer_review/program_files/Code1/obj/Debug/net8.0/Code1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Code1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04d9e412620a0f3b70c5450ad548cbc8915e36f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e657a8edea0cff74582c9098a18c049e52ffa92c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Code1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Code1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
