107efb8bd7850cf1326b99b22cd716b06e1fdff9
Breezy <114720008+breezy-codes@users.noreply.github.com>
2024-09-17 14:27:35 +1000

    Peer Review

---
+++ 14_SK_camera_peer_review/README.md
@@ -0,0 +1,15 @@
+# Peer Review Code Testing
+
+I did a peer review for the pull request found here [here](https://github.com/thoth-tech/splashkit.io-starlight/pull/183), I reviewed the adding in C# OOP code, left a request change on the review so the comments in the code would match with the other code blocks on the specific guide.
+
+Tested both the top level which was unchanged, with the OOP to ensure it runs correctly which it did.
+
+## Code Testing
+
+### C# OOP
+
+![alt text](images/run1.png)
+
+### C# Top Level
+
+![alt text](images/run2.png)
---
+++ 14_SK_camera_peer_review/program_files/Code1/Program.cs
@@ -0,0 +1,53 @@
+﻿using SplashKitSDK;
+
+namespace CameraGame
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            const int ScreenWidth = 800;
+            const int ScreenHeight = 800;
+            const int ScreenBorder = 100;
+            const double PlayerSpeed = 3;
+
+            Window window = SplashKit.OpenWindow("Camera Test", ScreenWidth, ScreenHeight);
+            double playerX = ScreenWidth / 2;
+            double playerY = ScreenHeight / 2;
+            double cameraX = 0;
+            double cameraY = 0;
+
+            while (!window.CloseRequested)
+            {
+                // Handle input
+                if (SplashKit.KeyDown(KeyCode.LeftKey)) playerX -= PlayerSpeed;
+                if (SplashKit.KeyDown(KeyCode.RightKey)) playerX += PlayerSpeed;
+                if (SplashKit.KeyDown(KeyCode.DownKey)) playerY += PlayerSpeed;
+                if (SplashKit.KeyDown(KeyCode.UpKey)) playerY -= PlayerSpeed;
+
+                // Update camera position
+                double leftEdge = cameraX + ScreenBorder;
+                double rightEdge = leftEdge + ScreenWidth - 2 * ScreenBorder;
+                double topEdge = cameraY + ScreenBorder;
+                double bottomEdge = topEdge + ScreenHeight - 2 * ScreenBorder;
+
+                if (playerY < topEdge) cameraY += topEdge - playerY;
+                else if (playerY > bottomEdge) cameraY += bottomEdge - playerY;
+
+                if (playerX < leftEdge) cameraX += leftEdge - playerX;
+                else if (playerX > rightEdge) cameraX += rightEdge - playerX;
+
+                // Draw everything
+                window.Clear(Color.Black);
+                window.FillRectangle(Color.Gray, 0 - cameraX, 0 - cameraY, ScreenWidth, 50);
+                window.DrawText("HUD", Color.White, 10 - cameraX, 10 - cameraY);
+                window.DrawText($"Camera Position: {cameraX:0.00000}:{cameraY:0.00000}", Color.White, 10 - cameraX, 30 - cameraY);
+                window.FillCircle(Color.Yellow, playerX - cameraX, playerY - cameraY, 20);
+                window.FillRectangle(Color.White, 400 - cameraX, 200 - cameraY, 10, 10);
+                SplashKit.RefreshScreen(60);
+            }
+
+            SplashKit.CloseAllWindows();
+        }
+    }
+}
