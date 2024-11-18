268c39e918e0a543a880d06a6993b10797d1346f
Breezy <114720008+breezy-codes@users.noreply.github.com>
2024-11-19 09:45:13 +1100

    Conversions

--- trimester-2/3_restful_API_Call/program_files/ex1/obj/Debug/net8.0/ex1.AssemblyInfo.cs
+++ trimester-2/3_restful_API_Call/program_files/ex1/obj/Debug/net8.0/ex1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ex1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef61efe08f910e4c75ae9c86eee33e7f3e5866a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f10a0051ab9c9418ee72185b63bcd46eecfe1dd6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ex1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ex1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
