4f1332046a456ad0a770e5d1d2b90533bce69321
Breezy <114720008+breezy-codes@users.noreply.github.com>
2024-11-24 10:41:18 +1100

    usage examples

--- 0-usage-examples/20-fileasstring/program_files/file.py
+++
@@ -1,8 +0,0 @@
-from splashkit import *
-
-# Read the file content as a string
-file_content = file_as_string("hello.json", resource_kind.json_resource)
-
-# Display the content in the console
-write_line("File Contents:")
-write_line(file_content)
