2898a6e4ff8683d42d939fdd5297444ffc581a2c
Breezy <114720008+breezy-codes@users.noreply.github.com>
2024-12-08 16:03:48 +1100

    JSON usage examples

--- 0-json/10-readnumber/program_files/Project_top/Program.cs
+++ 0-json/10-readnumber/program_files/Project_top/Program.cs
@@ -1,1 +1,15 @@
-﻿using static SplashKitSDK.SplashKit;
+﻿using SplashKitSDK;
+using static SplashKitSDK.SplashKit;
+
+// Create a JSON object with a float number
+Json json_obj = CreateJson();
+JsonSetNumber(json_obj, "temperature", 36.66);
+
+// Read the number value from the JSON object
+float temperature = JsonReadNumber(json_obj, "temperature");
+
+// Display the number value
+WriteLine("Temperature: " + temperature.ToString());
+
+// Free the JSON object
+FreeJson(json_obj);
